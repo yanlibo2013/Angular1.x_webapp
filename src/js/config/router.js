47e24b49c071cd60e4d3447b9f97836227b8f5cf
henry <yanlibo2010@163.com>
2017-06-12 14:37:21 +0800

    add dev template

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -6,47 +6,5 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function ($s
         templateUrl: 'view/home.html',
         controller: 'homeCtrl'
     })
-    $urlRouterProvider.otherwise('main');
-    // $stateProvider.state('main', {
-    //     url: '/main',
-    //     templateUrl: 'view/main.html',
-    //     controller: 'mainCtrl'
-    // }).state('search', {
-    //     url: '/search',
-    //     templateUrl: 'view/search.html',
-    //     controller: 'searchCtrl'
-    // }).state('mine', {
-    //     url: '/mine',
-    //     templateUrl: 'view/mine.html',
-    //     controller: 'mineCtrl'
-    // }).state('login', {
-    //     url: '/login',
-    //     templateUrl: 'view/login.html',
-    //     controller: 'loginCtrl'
-    // }).state('register', {
-    //     url: '/register',
-    //     templateUrl: 'view/register.html',
-    //     controller: 'registerCtrl'
-    // }).state('jobs',{
-    //     url: '/jobs/:id',
-    //     templateUrl: 'view/jobs.html',
-    //     controller: 'jobsCtrl'
-    // }).state('company',{
-    //     url: '/company/:id',
-    //     templateUrl: 'view/company.html',
-    //     controller: 'companyCtrl'
-    // }).state('post',{
-    //     url: '/post',
-    //     templateUrl: 'view/post.html',
-    //     controller: 'postCtrl'
-    // }).state('favorite',{
-    //     url: '/favorite',
-    //     templateUrl: 'view/favorite.html',
-    //     controller: 'favoriteCtrl'
-    // }).state('pass',{
-    //     url: '/pass',
-    //     templateUrl: 'view/pass.html',
-    //     controller: 'passCtrl'
-    // });
-    // $urlRouterProvider.otherwise('main');
+    $urlRouterProvider.otherwise('home');
 }]);
--- src/js/directive/footer.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-angular.module('app').directive('appFooter', [function(){
-    return {
-        restrict: 'AE',
-        replace: true,
-        templateUrl: 'view/template/footer.html'
-    }
-}]);
