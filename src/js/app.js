73c260c7c10b0093291b8acc3c5d86998a760036
henry <yanlibo2010@163.com>
2017-06-12 17:42:52 +0800

    add ngtouch module

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,4 @@
  * Created by tenderq on 2017-03-28.
  */
 'use strict'
-angular.module('app', ['ui.router', 'ngCookies', 'validation']);
+angular.module('app', ['ui.router', 'ngCookies', 'validation', 'ngTouch']);
--- src/js/controller/homeCtrl.js
+++ src/js/controller/homeCtrl.js
@@ -1,5 +1,11 @@
 'use strict'
 
-angular.module('app').controller('homeCtrl', ['$scope','$http','cache',function($scope,$http,cache){
+angular.module('app').controller('homeCtrl', ['$scope', '$http', 'cache', function($scope, $http, cache) {
+
+    $scope.onTap = function() {
+        alert("tap");
+        // console.log($scope.event); // the event object
+        // console.log('tap event called');
+    }
 
 }]);
